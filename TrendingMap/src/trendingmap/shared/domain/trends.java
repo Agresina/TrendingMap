ab125fd68087665637d63725bbe25b85b8e46b8d
Agresina <agresina@gmail.com>
2015-05-07 13:30:25 +0200

    Librerias rest importadas

--- TrendingMap/src/trendingmap/shared/domain/trends.java
+++ TrendingMap/src/trendingmap/shared/domain/trends.java
@@ -2,6 +2,39 @@ package trendingmap.shared.domain;
 
 import java.io.Serializable;
 
-public class trends implements Serializable{
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class trends implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -241719118465914770L;
+	
+	private	String name;
+	private String query;
+	private String url;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getQuery() {
+		return query;
+	}
+	public void setQuery(String query) {
+		this.query = query;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
 }
